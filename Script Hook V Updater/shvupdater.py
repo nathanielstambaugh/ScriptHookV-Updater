55e250177c769add8026749e1dc0b8d30ad5e324
Nathaniel Stambaugh <macdaddy101@icloud.com>
2022-05-05 09:54:19 -0400

    Trimmed down unused dependencies and added computer hostname check for string declarations

--- Script Hook V Updater/shvupdater.py
+++ Script Hook V Updater/shvupdater.py
@@ -1,32 +1,39 @@
-import requests
+from requests import get
 from bs4 import BeautifulSoup
 from win32api import GetFileVersionInfo, LOWORD, HIWORD
-import ctypes
+from ctypes import windll
 import zipfile
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.edge.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
-import time
-import os
-import shutil
+from time import sleep
+from os import remove, path
+from shutil import rmtree, move
+from socket import gethostname
 
-# String declarations for the various paths
+# String declarations for the various paths based on computer name
+if gethostname() == "STRIX" or gethostname() == "KG348":
+    steam_s = r"C:\test\ScriptHookV.dll"
+    zip_s = "bin/ScriptHookV.dll"
+    desktop = r"C:\Users\nstam\Desktop"
+    gamedir = r"C:\test"
+    desktop_s = r"C:\Users\nstam\Desktop\bin\ScriptHookV.dll"
+    bin_folder = r"C:\Users\nstam\Desktop\bin"
+    downloads = r"C:\Users\nstam\Downloads\ScriptHookV_"
+elif gethostname() == "DigitalStorm-PC":
+    steam_s = r"D:\SteamLibrary\steamapps\common\Grand Theft Auto V\ScriptHookV.dll"
+    desktop = r"C:\Users\Admin\Desktop"
+    gamedir = r"D:\SteamLibrary\steamapps\common\Grand Theft Auto V"
+    desktop_s = r"C:\Users\Admin\Desktop\bin\ScriptHookV.dll"
+    bin_folder = r"C:\Users\Admin\Desktop\bin"
+    downloads = r"C:\Users\Admin\Downloads\ScriptHookV_"
 
-#steam_s = r"D:\SteamLibrary\steamapps\common\Grand Theft Auto V\ScriptHookV.dll"
-steam_s = r"C:\test\ScriptHookV.dll"
-zip_s = "bin/ScriptHookV.dll"
-desktop = r"C:\Users\nstam\Desktop"
-# desktop = r"C:\Users\Admin\Desktop"
-# gamedir = r"D:\SteamLibrary\steamapps\common\Grand Theft Auto V"
-gamedir = r"C:\test"
-# desktop_s = r"C:\Users\Admin\Desktop\bin\ScriptHookV.dll"
-desktop_s = r"C:\Users\nstam\Desktop\bin\ScriptHookV.dll"
 
 # This function retrieves the most recent version number available from the site hosting it
 def getwebver():
-    page = requests.get('http://www.dev-c.com/GTAV/scripthookv')
+    page = get('http://www.dev-c.com/GTAV/scripthookv')
     soup = BeautifulSoup(page.text, "html.parser")
     anchors = soup.select("table tbody tr td table.tftablew td")
     return anchors[1].text.translate({ord("v"): None})
@@ -44,7 +51,7 @@ def versiontuple(v):
 
 # This function allows access to the windows messagebox to provide feedback to the user
 def mbox(text, title, style):
-    return ctypes.windll.user32.MessageBoxW(0, text, title, style)
+    return windll.user32.MessageBoxW(0, text, title, style)
 
 # This function will download the zip file from the hosting site
 def download():
@@ -59,7 +66,7 @@ def download():
         wait.until(expected_conditions.element_to_be_clickable((By.LINK_TEXT, "Download")))
         driver.find_element(by=By.LINK_TEXT, value="Download").click()
         # print("Element was clicked....\n")
-        time.sleep(10)
+        sleep(10)
     except:
         # print("Element wasn't clicked\n")
         exit()
@@ -69,28 +76,25 @@ def extractzip(filename):
     try:
         with zipfile.ZipFile(filename) as zip:
             zip.extract(zip_s, desktop)
-            print(filename + " extracted successfully!\n")
-        if os.path.exists(steam_s):
-            os.remove(steam_s)
-        shutil.move(desktop_s, gamedir)
-        shutil.rmtree(r"C:\Users\nstam\Desktop\bin")
-        # shutil.rmtree(r"C:\Users\Admin\Desktop\bin")
+            # print(filename + " extracted successfully!\n")
+        if path.exists(steam_s):
+            remove(steam_s)
+        move(desktop_s, gamedir)
+        rmtree(bin_folder)
         # print("files have been moved!\n")
     except Exception:
         mbox("Couldn't find " + filename, "Script Hook Updater", 0)
         exit()
 
-
+shzip = downloads + getwebver() + ".zip"
 try:
-    shzip = r"C:\Users\nstam\Downloads\ScriptHookV_" + getwebver() + ".zip"
-    # shzip = r"C:\Users\Admin\Downloads\ScriptHookV_" + getwebver() + ".zip"
     if versiontuple(getwebver()) > versiontuple(getlocalver(steam_s)):
         decision = mbox("Update available! Would you like to update?", "Script Hook Updater", 4)
         if decision == 6:
             download()
             extractzip(shzip)
             mbox("ScriptHookV has been updated successfully!", "Script Hook Updater", 0)
-            os.remove(shzip)
+            remove(shzip)
     else:
         mbox("No updates at this time", "Script Hook Updater", 0)
 except:
@@ -98,4 +102,4 @@ except:
     download()
     extractzip(shzip)
     mbox("ScriptHookV has been downloaded successfully!", "Script Hook Updater", 0)
-    os.remove(shzip)
+    remove(shzip)
